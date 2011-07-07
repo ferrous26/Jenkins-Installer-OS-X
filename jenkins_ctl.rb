45821fc22816ecdec747e8781b6b1c92b7dc3e1b
Mark Rada <mrada@marketcircle.com>
2011-07-07 00:20:00 -0400

    Update jenkins_ctl to not require macruby

--- jenkins_ctl.rb
+++ jenkins_ctl.rb
@@ -1,14 +1,13 @@
-#!/usr/bin/env macruby
+#!/usr/bin/env ruby
 
 STATUS_NOT_RUNNING  = 0
 STATUS_RUNNING      = 1
 LAUNCHD_LABEL       = 'org.jenkins-ci.jenkins'
 LAUNCHD_DIRECTORY   = '/Library/LaunchDaemons'
 LAUNCHD_FILE        = "#{LAUNCHD_LABEL}.plist"
-LAUNCHD_PATH        = File.join( LAUNCHD_DIRECTORY, LAUNCHD_FILE )
+LAUNCHD_PATH        = File.join(LAUNCHD_DIRECTORY, LAUNCHD_FILE)
 JENKINS_INSTALL_DIR = '/Library/Application Support/Jenkins'
 
-framework 'Foundation'
 require   'strscan'
 require   'fileutils'
 
@@ -16,7 +15,7 @@ include FileUtils
 
 
 unless ENV['USER'] == 'root'
-  NSLog('You need to run this script as root.')
+  $stderr.puts 'You need to run this script as root.'
   exit
 end
 
@@ -32,23 +31,23 @@ end
 case ARGV[0]
 when 'status'
   msg = (status == STATUS_NOT_RUNNING) ? 'not running' : 'running'
-  NSLog('Jenkins is ' + msg)
+  $stderr.puts 'Jenkins is ' + msg
 
 when 'stop'
   unless status == STATUS_NOT_RUNNING
     `sudo launchctl unload #{LAUNCHD_PATH}`
     rm LAUNCHD_PATH
   else
-    NSLog('Jenkins is not running')
+    $stderr.puts 'Jenkins is not running'
     exit
   end
 
 when 'start'
   if status == STATUS_RUNNING
-    NSLog('Jenkins is already running')
+    $stderr.puts 'Jenkins is already running'
     exit
   end
-  unless File::exists?( LAUNCHD_PATH )
+  unless File.exists? LAUNCHD_PATH
     ln_s File.join(JENKINS_INSTALL_DIR, LAUNCHD_FILE), LAUNCHD_PATH
   end
   `sudo launchctl load #{LAUNCHD_PATH}`
